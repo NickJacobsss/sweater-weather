350fa1e6fb864965c91b42fd673e414463d93b5c
Nick Jacobs <nickjacobsatc@gmail.com>
2022-08-06 14:23:31 -0500

    Test: creates json with hourly weather

---
+++ spec/poros/hourly_weather_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe HourlyWeather do
+
+  it "creates object from JSON with hourly weather" do
+    forecast = JSON.parse(File.read('spec/fixtures/forecast_example.json'), symbolize_names: true)
+    hourly_weather = HourlyWeather.new(forecast[:hourly][0])
+    expect(hourly_weather.conditions).to eq("Rain")
+    expect(hourly_weather.icon).to eq("10d")
+    expect(hourly_weather.temp).to eq(83.86)
+    expect(hourly_weather.time).to eq("2022-08-06 13:00:00 -0500")
+  end
+
+end
