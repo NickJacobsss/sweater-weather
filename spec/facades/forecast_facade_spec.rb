d669b69e03be7ca05b66a95c1c647cd743054fab
Nick Jacobs <nickjacobsatc@gmail.com>
2022-08-06 14:43:33 -0500

    Test: returns current hourly and daily forecasts

---
+++ spec/facades/forecast_facade_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "ForecastFacade" do
+
+  it "returns current, hourly, and daily forecasts given a lat/long" do
+    dayton_forecast = ForecastFacade.get_forecast(39.760982,-84.192203)
+
+    expect(dayton_forecast).to include(:current,:daily,:hourly)
+    expect(dayton_forecast).to be_a Hash
+    expect(dayton_forecast[:daily]).to be_a Array
+    expect(dayton_forecast[:hourly]).to be_a Array
+
+    expect(dayton_forecast[:current]).to be_a CurrentWeather
+    expect(dayton_forecast[:daily][0]).to be_a DailyWeather
+    expect(dayton_forecast[:hourly][0]).to be_a HourlyWeather
+  end
+
+end
