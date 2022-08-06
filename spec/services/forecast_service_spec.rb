777e737aad5aefffa9603dcfec7bd4effa2db3f5
Nick Jacobs <nickjacobsatc@gmail.com>
2022-08-06 13:19:16 -0500

    Test: gives forecast info for city

--- spec/services/forecast_service_spec.rb
+++ spec/services/forecast_service_spec.rb
@@ -1,5 +1,12 @@
 require "rails_helper"
 
 RSpec.describe "Forecast API Service " do
-  
+
+  it 'gives forecast for lat/long of city' do
+    forecast = ForecastService.forecast(39.760982,-84.192203)
+    expect(forecast).to have_key :current
+    expect(forecast).to have_key :daily
+    expect(forecast).to have_key :hourly
+  end
+
 end
