28599c5cf8761f8a8591a40f3e7bb7445e75776c
Nick Jacobs <nickjacobsatc@gmail.com>
2022-08-07 16:34:50 -0500

    Finished Forecast added

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -4,15 +4,15 @@ class ForecastFacade
     all_forecasts = ForecastService.forecast(lat, long)
     current_forecast = CurrentWeather.new(all_forecasts)
 
-    daily_forecasts = all_forecasts[:daily][0..4].map do |daily|
+    daily_forecasts = all_forecasts[:daily].map do |daily|
       DailyWeather.new(daily)
     end
 
-    hourly_forecasts = all_forecasts[:hourly][0..7].map do |hourly|
+    hourly_forecasts = all_forecasts[:hourly].map do |hourly|
       HourlyWeather.new(hourly)
     end
 
     {current: current_forecast, daily: daily_forecasts, hourly: hourly_forecasts}
   end
-  
+
 end
