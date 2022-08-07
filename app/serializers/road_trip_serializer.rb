d2af2f730f5b1825ae74b81f1f621191b41169b5
Nick Jacobs <nickjacobsatc@gmail.com>
2022-08-07 18:08:16 -0500

    Feat: added impossible route serializer method

--- app/serializers/road_trip_serializer.rb
+++ app/serializers/road_trip_serializer.rb
@@ -1,19 +1,38 @@
 class RoadTripSerializer
 
-  def self.format_road_trip
-  {
-  "data": {
-    "id": null,
-    "type": "roadtrip",
-    "attributes": {
-      "start_city": ,
-      "end_city": ,
-      "travel_time":
-      "weather_at_eta": {
-        "temperature": ,
-        "conditions":
+  def self.format_road_trip(start, finish, forecast_time)
+    {
+    "data": {
+      "id": "null",
+      "type": "roadtrip",
+      "attributes": {
+        "start_city": start,
+        "end_city": finish,
+        "travel_time": forecast_time.travel_time,
+        "weather_at_eta": {
+          "temperature": forecast_time.finished_forecast[:temp],
+          "conditions": forecast_time.finished_forecast[:conditions]
+        }
       }
     }
   }
-}
+  end
+
+  def self.impossible_route
+    {
+    "data": {
+      "id": "null",
+      "type": "roadtrip",
+      "attributes": {
+        "start_city": start,
+        "end_city": finish,
+        "travel_time": "impossible",
+        "weather_at_eta": {
+          "temperature": "",
+          "conditions": ""
+        }
+      }
+    }
+  }
+  end
 end
