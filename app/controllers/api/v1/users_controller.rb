dea7546ccb8e3fe66afd229110bbdbed824a4358
Nick Jacobs <nickjacobsatc@gmail.com>
2022-08-06 19:56:19 -0500

    Fix: added 201 specific success code

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::UsersController < ApplicationController
   def create
       new_user = User.create(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation], api_key: SecureRandom.hex)
       if new_user.save
-        render json: UserSerializer.create_user(new_user)
+        render json: UserSerializer.create_user(new_user), status: 201
       else
         render json: {error: new_user.errors.full_messages.to_sentence}, status: 400
       end
