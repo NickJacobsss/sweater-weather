3898c9e60df7d816f9aff23541116ca93f3dff9d
Nick Jacobs <nickjacobsatc@gmail.com>
2022-08-07 16:38:28 -0500

    Work in progress, template created

---
+++ app/serializers/road_trip_serializer.rb
@@ -0,0 +1,19 @@
+class RoadTripSerializer
+
+  def self.format_road_trip
+  {
+  "data": {
+    "id": null,
+    "type": "roadtrip",
+    "attributes": {
+      "start_city": ,
+      "end_city": ,
+      "travel_time":
+      "weather_at_eta": {
+        "temperature": ,
+        "conditions":
+      }
+    }
+  }
+}
+end
