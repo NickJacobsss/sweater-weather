d0d5e832a1a0a56cfa255c67aaa8ec5ec20c614f
Nick Jacobs <nickjacobsatc@gmail.com>
2022-08-07 18:39:11 -0500

    Feat: added impossible route logic

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -5,14 +5,14 @@ class Api::V1::RoadTripController < ApplicationController
     finish = params[:destination]
     key = params[:api_key]
     if User.find_by(api_key: key)
+      begin
       forecast_and_time = RoadTripFacade.get_future_forecast(start,finish)
       render json: RoadTripSerializer.format_road_trip(start, finish, forecast_and_time)
-      # else
-      #   render json: RoadTripSerializer.impossible_route
-      # end
+      rescue NoMethodError
+      render json: RoadTripSerializer.impossible_route(start,finish)
+      end
     else
       render json: {error: "Unauthorized API Key"}, status: 400
     end
   end
-
 end
