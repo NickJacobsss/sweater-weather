7809f511a32d1d41be687b67c7371169fdb1ccf0
Nick Jacobs <nickjacobsatc@gmail.com>
2022-08-06 19:07:33 -0500

    Serializer: create user

---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,15 @@
+class UserSerializer
+
+  def self.create_user(user)
+    {
+      "data": {
+      "type": "users",
+      "id": "#{user.id}",
+      "attributes": {
+        "email": user.email,
+        "api_key": user.api_key
+        }
+      }
+    }
+  end
+end
