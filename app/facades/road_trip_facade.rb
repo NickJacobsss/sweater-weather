450ed4b89def05752c476162552cd7edff3760a1
Nick Jacobs <nickjacobsatc@gmail.com>
2022-08-07 18:07:38 -0500

    Refactor: renamed facade method

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,6 +1,6 @@
 class RoadTripFacade
 
-  def self.get_route(start,finish)
+  def self.get_future_forecast(start,finish)
     route_data = RouteService.get_route(start,finish)
     route = Route.new(route_data)
     lat_long = GeocodeFacade.city_location(finish)
