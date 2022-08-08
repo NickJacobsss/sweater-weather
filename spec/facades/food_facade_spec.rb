a817c17fa4f4040eb8e92fa9de81c9962bc62d93
Nick Jacobs <nickjacobsatc@gmail.com>
2022-08-08 12:26:04 -0500

    Refactor: removed spacing

--- spec/facades/food_facade_spec.rb
+++ spec/facades/food_facade_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "FoodFacade" do
   describe "happy path" do
-    it "returns relevant food info " do
+    it "returns relevant food info" do
       food_data = FoodFacade.get_info("dayton, oh", "chinese food")
       expect(food_data.name).to eq("Kung Fu Noodle")
       expect(food_data.address).to eq("2801 S Dixie Dr, Ste 3, Dayton, OH 45409")
