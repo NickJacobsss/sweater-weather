7ba61b1d398762eb199ddf75bbb7c620ae15839d
Nick Jacobs <nickjacobsatc@gmail.com>
2022-08-08 11:33:11 -0500

    Test: sad path for no location or food param given

--- spec/requests/api/v1/food_request_spec.rb
+++ spec/requests/api/v1/food_request_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Food Request" do
+  
   describe "happy path" do
     it "returns food and weather information for business location" do
       get "/api/v1/munchies?location=denver,co&food=chinese"
@@ -12,6 +13,20 @@ RSpec.describe "Food Request" do
       expect(data[:attributes][:forecast][:temperature]).to be_a Float
       expect(data[:attributes][:restaurant][:name]).to eq("Taste of China")
       expect(data[:attributes][:restaurant][:address]).to eq("1935 Federal Blvd, Denver, CO 80204")
+    end
+  end
+
+  describe "sad path" do
+    it "returns an error when no food param is given " do
+      get "/api/v1/munchies?location=denver,co"
+      data = JSON.parse(response.body, symbolize_names: true)
+      expect(data[:error]).to eq("please enter location & food type")
+    end
+
+    it "returns an error when no location param is given" do
+      get "/api/v1/munchies?food=chinese"
+      data = JSON.parse(response.body, symbolize_names: true)
+      expect(data[:error]).to eq("please enter location & food type")
     end
   end
 end
