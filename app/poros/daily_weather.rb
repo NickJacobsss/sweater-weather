5ea2aff36047e79c2fe8ea988235873f11ef13ec
Nick Jacobs <nickjacobsatc@gmail.com>
2022-08-08 12:25:50 -0500

    Refactor: added strftime to date/times

--- app/poros/daily_weather.rb
+++ app/poros/daily_weather.rb
@@ -2,9 +2,9 @@ class DailyWeather
   attr_reader :date, :sunrise, :sunset, :max_temp, :min_temp, :conditions, :icon, :temp
 
   def initialize(forecast_data)
-    @date = Time.at(forecast_data[:dt]).to_s
-    @sunrise = Time.at(forecast_data[:sunrise]).to_s
-    @sunset = Time.at(forecast_data[:sunset]).to_s
+    @date = Time.at(forecast_data[:dt]).strftime("%-d %b %Y").to_s
+    @sunrise = Time.at(forecast_data[:sunrise]).strftime("%H:%M").to_s
+    @sunset = Time.at(forecast_data[:sunset]).strftime("%H:%M").to_s
     @max_temp = forecast_data[:temp][:max]
     @min_temp = forecast_data[:temp][:min]
     @conditions = forecast_data[:weather][0][:main]
