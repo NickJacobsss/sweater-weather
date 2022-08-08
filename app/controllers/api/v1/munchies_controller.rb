b84c27baf077f77b4c17d94e75f01b0b7f67a82a
Nick Jacobs <nickjacobsatc@gmail.com>
2022-08-08 11:32:52 -0500

    Feat: added conditional statement to give error if both params not given

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -3,10 +3,14 @@ class Api::V1::MunchiesController < ApplicationController
   def index
     # location = params[:location]
     # food = params[:food]
-    food_info = FoodFacade.get_info(params[:location], params[:food])
-    lat_long = GeocodeFacade.city_location(params[:location])
-    forecast = ForecastFacade.get_forecast(lat_long.lat, lat_long.long)
-    render json: FoodSerializer.format_food(food_info, forecast, params[:location])
+    if params[:location].present? && params[:food].present?
+      food_info = FoodFacade.get_info(params[:location], params[:food])
+      lat_long = GeocodeFacade.city_location(params[:location])
+      forecast = ForecastFacade.get_forecast(lat_long.lat, lat_long.long)
+      render json: FoodSerializer.format_food(food_info, forecast, params[:location])
+    else
+      render status: :bad_request, json: { error: 'please enter location & food type' }
+    end 
   end
 
 end
